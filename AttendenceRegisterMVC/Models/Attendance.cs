fd3202627579bb7f7547d89c682c27404fe473f8
MzukisiTekeni <162095867+MzukisiTekeni@users.noreply.github.com>
2025-09-03 12:11:24 +0000

    erwgterhgeryerter

---
+++ AttendenceRegisterMVC/Models/Attendance.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AttendenceRegisterMVC.Models
+{
+    public class Attendance
+    {
+        public string SelectedOption { get; set; }
+        public List<SelectListItem> attendance { get; set; }
+    }
+}
